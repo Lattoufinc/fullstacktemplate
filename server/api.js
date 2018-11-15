83fbdd7a642b47cc66d290830858ae828bb37230
slattouf95 <lattoufsufian@gmail.com>
2018-11-15 11:42:59 -0800

    fixed unhandled promise error

--- server/api.js
+++ server/api.js
@@ -2,6 +2,25 @@ const dialogflow = require('dialogflow');
 
 const sessionClient = new dialogflow.SessionsClient();
 
+function checkType(results) {
+  if (results.fulfillmentText) {
+    console.log('detected simple response');
+    var type = 2;
+    var server = results.fulfillmentText;
+    return { server, type };
+  } else if (results.fulfillmentMessages[0].suggestions.suggestions[0].title) {
+    type = 1;
+    var server = results.fulfillmentMessages[0].suggestions.suggestions;
+    return { server, type };
+  }
+
+  if (result.intent) {
+    console.log(`  Intent: ${result.intent.displayName}`);
+  } else {
+    console.log(`  No intent matched.`);
+  }
+}
+
 async function sendTextMessageToDialogFlow(textMessage) {
   // Define session path
   const projectId = 'events-search-2565a'; //https://dialogflow.com/docs/agents#settings
@@ -22,12 +41,12 @@ async function sendTextMessageToDialogFlow(textMessage) {
 
   try {
     const responses = await sessionClient.detectIntent(request);
+
     console.log('DialogFlow.sendTextMessageToDialogFlow: Detected intent');
     const result = responses[0].queryResult;
-    console.log('type', responses[0].queryResult.fulfillmentMessages[0].suggestions.suggestions);
-    console.log(`  Query: ${result.queryText}`);
-    console.log(`  Response: ${result.fulfillmentText}`);
-    return responses;
+    const object = checkType(result);
+    console.log(object);
+    return object;
   } catch (err) {
     console.error('DialogFlow.sendTextMessageToDialogFlow ERROR:', err);
     throw err;
--- server/app.js
+++ server/app.js
@@ -17,24 +17,14 @@ const io = socketIO(server);
 
 io.on('connection', socket => {
   socket.on('fromClient', data => {
-    api.sendTextMessageToDialogFlow(data.client).then(res => {
-      let result;
-      let type;
-      if (res[0].queryResult.fulfillmentMessages[0].suggestions.suggestions[0].title) {
-        type = 1;
-        result = res[0].queryResult.fulfillmentMessages[0].suggestions.suggestions;
-      } else if (res[0].queryResult.fulfillmentText) {
-        console.log('detected simple response');
-        type = 2;
-        result = res[0].queryResult.fulfillmentText;
-      }
-      socket.emit('fromServer', { server: result, type });
-      if (result.intent) {
-        console.log(`  Intent: ${result.intent.displayName}`);
-      } else {
-        console.log(`  No intent matched.`);
-      }
-    });
+    api
+      .sendTextMessageToDialogFlow(data.client)
+      .then(res => {
+        console.log('results', res);
+
+        socket.emit('fromServer', res);
+      })
+      .catch(err => console.log(err));
   });
 });
 
